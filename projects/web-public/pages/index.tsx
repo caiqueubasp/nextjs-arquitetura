a57880f824434f0ca371887e96545b806a25da79
Caique Souza <caiqueubasp@gmail.com>
2023-10-13 18:36:11 -0300

    workspace and styled-components configs

---
+++ projects/web-public/pages/index.tsx
@@ -0,0 +1,11 @@
+import { sum } from "@alura/utils/math/sum";
+import { Text } from "@alura/design-system/components/Text";
+import React from 'react';
+export default function HomeScreen() {
+  return (
+    <main>
+      <Text tag="h1">Home</Text>
+      <Text tag="p">Importando modulo local '@alura/utils/math/sum' {sum(6,2)}</Text>
+    </main>
+  );
+}
